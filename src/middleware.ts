de0533f0801133c0c0083bd6fe80c9c6902d5787
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-02-05 10:35:48 +0700

    fix: set both domain and subdomain to authorized parties
    attempt to include the authorized party for security

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+export default clerkMiddleware({
+  authorizedParties: ['https://swattlasagna.com', 'https://www.swattlasagna.com'],
+})
 
 export const config = {
   matcher: [
