8203c1c15a6adb99d93b799c1a967eeb5bb7ce79
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-02-05 11:57:01 +0700

    fix: add user button to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import ModeSwitch from "@/components/ModeSwitch";
 import theme from "@/theme";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider, UserButton } from '@clerk/nextjs'
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import CssBaseline from "@mui/material/CssBaseline";
 import InitColorSchemeScript from "@mui/material/InitColorSchemeScript";
 import { ThemeProvider } from "@mui/material/styles";
 import * as React from "react";
-import { SignOutButton } from "@/components/SignOutButton";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
@@ -19,7 +18,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
               {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
               <CssBaseline />
               <ModeSwitch />
-              <SignOutButton />
+              <UserButton />
               {props.children}
             </ThemeProvider>
           </AppRouterCacheProvider>
